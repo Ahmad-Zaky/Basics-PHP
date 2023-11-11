dfe97d0aa9ee06d0ac8376ceab03a399561e38a0
Fouad <ahmed6mohamed6@gmail.com>
2023-11-11 09:49:49 +0200

    refactor migration file

---
+++ app/Commands/kernal.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ZAKY_START', microtime(true));
+
+require dirname(__DIR__) .'/../vendor/autoload.php';
+
+require dirname(__DIR__) .'/bootstrap.php';
--- app/Commands/migrate.php
+++ app/Commands/migrate.php
@@ -1,9 +1,5 @@
 <?php
 
-define('ZAKY_START', microtime(true));
-
-require dirname(__DIR__) .'/../vendor/autoload.php';
-
-require dirname(__DIR__) .'/bootstrap.php';
+require dirname(__DIR__) .'/Commands/kernal.php';
 
 migrate();
